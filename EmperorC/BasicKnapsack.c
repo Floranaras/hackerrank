bf2b9c6581af4655d20ff530103289d49058f1bc
Floranaras <carlosranara0@gmail.com>
2025-10-06 21:23:24 +0800

    fix segfault again

--- EmperorC/BasicKnapsack.c
+++ EmperorC/BasicKnapsack.c
@@ -1,46 +1,65 @@
 #include <stdio.h>
+#define MOD 1000000007
+#define MAX_C 100000
+#define MAX_N 1000
 
-long long min(long long a, long long b)
+void init_dp(long long dp[], int c)
 {
-    if (a < b)
-        return a;
-
-    return b;
-}
-
-void update_dp_state(long long *dp0, long long *dp1, int a, int b, int k)
-{
-    long long new_dp0 = min(*dp0 + a, *dp1 + k + a);
-    long long new_dp1 = min(*dp1 + b, *dp0 + k + b);
-    
-    *dp0 = new_dp0;
-    *dp1 = new_dp1;
-}
-
-long long solve_assembly_lines(int n, int k)
-{
-    int a, b;
-    scanf("%d %d", &a, &b);
-    
-    long long dp0 = a;
-    long long dp1 = b;
-    
     int i;
-    for (i = 1; i < n; i++)
+    dp[0] = 1;
+    for(i = 1; i <= c; i++)
+        dp[i] = 0;
+}
+
+void lambda_function(long long dp[], int wt, int c)
+{
+    int i;
+    long long temp[MAX_C + 1];
+    
+    for(i = 0; i <= c; i++)
     {
-        scanf("%d %d", &a, &b);
-        update_dp_state(&dp0, &dp1, a, b, k);
+        temp[i] = dp[i];
+        if(i >= wt)
+            temp[i] = (temp[i] + dp[i - wt]) % MOD;
     }
     
-    return min(dp0, dp1);
+    for(i = 0; i <= c; i++)
+        dp[i] = temp[i];
+}
+
+void reduce(long long dp[], int n, int c)
+{
+    int i, wt;
+    
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &wt);
+        lambda_function(dp, wt, c);
+    }
+}
+
+long long sum_dp(long long dp[], int c)
+{
+    int i;
+    long long sum = 0;
+    
+    for(i = 0; i <= c; i++)
+        sum = (sum + dp[i]) % MOD;
+        
+    return sum;
 }
 
 int main()
 {
-    int n, k;
-    scanf("%d %d", &n, &k);
+    int n, c;
+    long long dp[MAX_C + 1];
     
-    printf("%lld\n", solve_assembly_lines(n, k));
+    scanf("%d %d", &n, &c);
+    
+    init_dp(dp, c);
+    reduce(dp, n, c);
+    
+    printf("%lld\n", sum_dp(dp, c));
     
     return 0;
 }
